dac4dae75683c369b1d539af9928660d292596dc
Gennaro Imperatore <gennaro.imperatore@geo-sport.co.uk>
2019-01-23 11:05:59 +0000

    Add project files.

---
+++ AnimalDiseaseQueryPage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AnimalDiseaseQueryPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
