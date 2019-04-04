9c2326ed11c04062ac753aaf3c54440c9a5e22fd
Gennaro Imperatore <gennaro.imperatore@geo-sport.co.uk>
2019-04-04 16:20:30 +0100

    started on diagnosis page

---
+++ AnimalDiseaseQueryWebApp/Models/DiagnoseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnimalDiseaseQueryWebApp.Models
+{
+    public class DiagnoseViewModel
+    {
+        List<Animal> animals;
+
+    }
+}
