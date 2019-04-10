5ad5a289726a0a71cfb41924f524cf6656956154
Gennaro Imperatore <gennaro.imperatore@geo-sport.co.uk>
2019-04-10 15:07:23 +0100

    further progress on signs

--- AnimalDiseaseQueryWebApp/Controllers/DiagnoseController.cs
+++ AnimalDiseaseQueryWebApp/Controllers/DiagnoseController.cs
@@ -12,21 +12,23 @@ namespace AnimalDiseaseQueryWebApp.Controllers
 {
     public class DiagnoseController : Controller
     {
-        List<Sign> signsForAnimal = new List<Sign>();
-
+        Dictionary<Animal, List<Sign>> signsForAnimal = new Dictionary<Animal, List<Sign>>();
+        
         // GET: Diagnose
         public ActionResult Index(ADDB context, DiagnoseViewModel model)
         {
            
             model.animals = context.Animals.ToList();
 
+            LoadSignsMasterList(context); // the idea is that everytime the page is refreshed the master list for signs is loaded
+
             return View(model);
         }
 
-        public void LoadSignsMasterList()
+        public void LoadSignsMasterList(ADDB context)
         {
             string extension = ".xlsx";
-            string filename = "data";
+            string filename = "master_list";
             string path = Server.MapPath(@"~/Files/" + filename + extension);
 
             try
@@ -34,10 +36,61 @@ namespace AnimalDiseaseQueryWebApp.Controllers
                 Excel.Application app = new Excel.Application();
 
                 Excel.Workbook WB = app.Workbooks.Open(path);
-            }
-            catch(Exception e)
-            {
 
+                Excel.Worksheet signsWorkSheet = WB.Worksheets["Signs core"];
+
+                Excel.Range usedCells = signsWorkSheet.UsedRange;
+                object[,] valueArray = (object[,])usedCells.get_Value(
+                                        XlRangeValueDataType.xlRangeValueDefault);
+                int rowsLength = valueArray.GetLength(0);
+                int columnsLength = valueArray.GetLength(1);
+
+                for (int c = 2; c <= columnsLength; c++)
+                {
+
+                    string name = (string)valueArray[1, c];
+                    if (name == null)
+                        continue;
+                    name = name.ToUpper(); // name needs to be uppercase
+                    List<int> ids = new List<int>();
+
+                    var animals = context.Animals.Where(n => n.Name.Contains(name)).ToList();
+
+                    foreach (Animal a in animals)
+                    {
+                        List<Sign> signList = new List<Sign>();
+
+                        for (int r = 2; r <= rowsLength; r++)
+                        {
+                            string signName = (string)valueArray[r, 1];
+                            if (signName == null)
+                                continue;
+                            string columnValue = (string)valueArray[r, c];
+                            if (columnValue == null)
+                                continue;
+                            if (columnValue.Equals("X"))
+                            {
+
+                                Sign sign = context.Signs.Where(s => s.Name.Contains(signName.ToUpper())).ToList()[0];
+                                signList.Add(sign);
+                            }
+
+
+                        }
+                        signsForAnimal.Add(a, signList);
+                    }
+                }
+
+                //close the workbook and the app 
+                WB.Close();
+
+                Marshal.ReleaseComObject(WB);
+                Marshal.ReleaseComObject(app);
+
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
             }
         }
 
