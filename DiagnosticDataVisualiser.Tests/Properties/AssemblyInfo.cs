d39330486b24e7e265883c48cf5409b278ccd200
Gennaro Imperatore <gennaro.imperatore@geo-sport.co.uk>
2019-05-15 11:43:41 +0100

    preparatory work for unit testing

---
+++ DiagnosticDataVisualiser.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DiagnosticDataVisualiser.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("DiagnosticDataVisualiser.Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("579b9acb-e1db-4a44-b4b1-b42f21c39eeb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DiagnosticDataVisualiser.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DiagnosticDataVisualiser;
+
+using DiagnosticDataVisualiser.Controllers;
+
+namespace DiagnosticDataVisualiser.Tests
+{
+    
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            HomeController controller = new HomeController();
+            
+        }
+    }
+}
