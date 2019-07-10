d38d2ae2c84a318ea013e379bca11a3979f54e1b
Gennaro Imperatore <gennaro.imperatore@geo-sport.co.uk>
2019-07-10 17:18:37 +0200

    created scaffolding for admin page

---
+++ DiagnosticDataVisualiser/Controllers/AdminPageController.cs
@@ -0,0 +1,25 @@
+﻿using DiagnosticDataVisualiser.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiagnosticDataVisualiser.Controllers
+{
+    public class AdminPageController : Controller
+    {
+        // GET: AdminPage
+        public ActionResult Index(AdminPageViewModel model)
+        {
+            model.currentUser = User.Identity.Name;
+            var userManager =HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var u = userManager.Users;
+            
+
+            return View(model);
+        }
+    }
+}
---
+++ DiagnosticDataVisualiser/Models/AdminPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiagnosticDataVisualiser.Models
+{
+
+    public class AdminPageViewModel
+    {
+        public string currentUser { get; set; }
+        public List<ApplicationUser> registeredUsers { get; set; }
+        
+    }
+}
