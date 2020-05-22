972ae0eedebc0508f689d6946b09fe22a094d1c5
Gennaro Imperatore <gennaro.imperatore@geo-sport.co.uk>
2020-05-22 10:37:55 +0100

    made a start on auto sync

---
+++ VetEddieAutomaticCopier/Copiercs.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace VetEddieAutomaticCopier
+{
+    class Copier
+    {
+        public void CopyNewCases()
+        {
+            /*  const string driver = "DRIVER={MySQL ODBC 8.0 ANSI Driver};";
+              const string server = "SERVER =188.121.44.186;";
+              const string D3Fdatabase = "DATABASE=DebugFramework;";
+              const string vetEddieDatabase = "DATABASE=veteddie_eddie;";
+              const string user = "UID=gennaro2;";
+              const string password = "PASSOWORD=sha9tTer;";
+              const string option = "OPTION=3;";
+              const string D3FconnectionString = driver + server + D3Fdatabase + user + password + option;
+              const string vetEddieConnectionString = driver + server + vetEddieDatabase + user + password + option;*/
+
+
+            OdbcConnection d3fConnection = new OdbcConnection("DSN=d3fdb");
+
+            OdbcConnection vetEddieConnection = new OdbcConnection("DSN=veteddie");
+            try
+            {
+
+                Console.WriteLine("Attempting connection to D3F vet eddie");
+                d3fConnection.Open();
+                Console.WriteLine("Attempting connection to vet eddie");
+                vetEddieConnection.Open();
+
+                OdbcCommand d3fQuery = new OdbcCommand("SELECT COUNT(*) FROM cases", d3fConnection);
+                OdbcCommand vetEddieQuery = new OdbcCommand("SELECT COUNT(*) FROM cases", vetEddieConnection);
+
+                // int vetEddieCount = 
+                Console.WriteLine("There are currently {0} cases in d3f vet eddie", d3fQuery.ExecuteScalar());
+                Console.WriteLine("There are currently {0} cases in original vet eddie", vetEddieQuery.ExecuteScalar());
+
+                OdbcCommand d3fTableCommand = new OdbcCommand("SHOW TABLES", d3fConnection);
+                OdbcCommand vetEddieTableCommand = new OdbcCommand("SHOW TABLES", vetEddieConnection);
+
+                // declare reader objects for tables and columns
+                OdbcDataReader d3fTableReader;
+                
+
+                OdbcDataReader vetEddieTableReader;
+               
+
+                // queries that return result set are executed by ExecuteReader()
+                // If you are to run queries like insert, update, delete then
+                // you would invoke them by using ExecuteNonQuery() 
+                d3fTableReader = d3fTableCommand.ExecuteReader();
+                vetEddieTableReader = vetEddieTableCommand.ExecuteReader();
+
+
+                List<String> d3fTableNames = new List<string>();
+                List<String> vetEddieTableNames = new List<String>();
+
+                int d3FTableCount = 0;
+                int vetEddieTableCount = 0;
+
+                Console.WriteLine("Reading d3f vet eddie table structure...");
+
+                while (d3fTableReader.Read())
+                {
+
+                    Console.Write(d3fTableReader.GetValue(0)+" ");
+                    d3fTableNames.Add(d3fTableReader.GetValue(0).ToString());
+                }
+                d3FTableCount = d3fTableNames.Count;
+
+                Console.WriteLine("There are {0} tables in d3f version of vet eddie", d3FTableCount);
+
+                Console.WriteLine("Reading vet eddie table structure...");
+
+                while (vetEddieTableReader.Read())
+                {
+                    Console.Write(vetEddieTableReader.GetValue(0)+" ");
+                    vetEddieTableNames.Add(vetEddieTableReader.GetValue(0).ToString());
+                }
+                vetEddieTableCount = vetEddieTableNames.Count;
+
+                Console.WriteLine("There are {0} tables in original version of vet eddie", vetEddieTableCount);
+
+                if(vetEddieTableCount == d3FTableCount)
+                {
+                    Console.WriteLine("Table count matches");
+                    bool tableNamesMatch =vetEddieTableNames.SequenceEqual(d3fTableNames);
+
+                    if(tableNamesMatch)
+                    {
+                        Console.WriteLine("The table names match");
+
+                        Console.WriteLine("Starting to sync table data");
+                        Console.WriteLine("Checking data integrity before attempting sync...");
+                        bool integrityOK = true;
+
+                        foreach (String tableName in vetEddieTableNames)
+                        {
+                           
+                            Console.WriteLine("Checking integrity for table {0} ", tableName);
+
+                            OdbcCommand vetEddieColumnCommand = new OdbcCommand("SHOW COLUMNS IN " + tableName, vetEddieConnection);
+                            OdbcCommand d3fColumnCommand = new OdbcCommand("SHOW COLUMNS IN " + tableName, d3fConnection);
+
+                            OdbcDataReader vetEddieColumnReader = vetEddieColumnCommand.ExecuteReader();
+                            OdbcDataReader d3fColumnReader = d3fColumnCommand.ExecuteReader();
+
+                            List<String> d3ftableColumns = new List<string>();
+                            List<String> vetEddieTableColumns = new List<string>();
+
+                            while(vetEddieColumnReader.Read())
+                            {
+                                vetEddieTableColumns.Add(vetEddieColumnReader.GetValue(0).ToString());
+                            }
+
+                            int vetEddieColumnCount = vetEddieTableColumns.Count;
+
+                            while (d3fColumnReader.Read())
+                            {
+                                d3ftableColumns.Add(d3fColumnReader.GetValue(0).ToString());
+                            }
+
+                            int d3fColumnCount = d3ftableColumns.Count;
+
+                            Console.WriteLine("Found {0} columns in d3f version of vet eddie for table {1}", d3fColumnCount,tableName);
+                            Console.WriteLine("Found {0} columns in original vet eddie for table {1}", vetEddieColumnCount, tableName);
+                            
+
+                            
+                            vetEddieColumnReader.Close();
+                            d3fColumnReader.Close();
+
+
+
+                        }
+
+                        if(integrityOK)
+                        {
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Integtrity check failed please check!");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Some table names have been changed please check!");
+                   
+                        Console.WriteLine("Stopping exection");
+
+                        return;
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Table count does not match please check!");
+                    Console.WriteLine("Stopping execution");
+                }
+
+
+                d3fTableReader.Close();
+                vetEddieTableReader.Close();
+
+                d3fConnection.Close();
+                vetEddieConnection.Close();
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                d3fConnection.Close();
+                vetEddieConnection.Close();
+
+            }
+
+        }
+
+       
+
+
+
+    }
+}
---
+++ VetEddieAutomaticCopier/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetEddieAutomaticCopier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Copier c = new Copier();
+            c.CopyNewCases();
+            Console.ReadLine();
+        }
+    }
+}
