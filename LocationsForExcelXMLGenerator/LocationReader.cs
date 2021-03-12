37a217850a2c2d5e7ac500c0dc646a519adf270b
Gennaro Imperatore <spikeg88@hotmail.it>
2021-03-12 09:17:55 +0000

    fixed parser

--- LocationsForExcelXMLGenerator/LocationReader.cs
+++ LocationsForExcelXMLGenerator/LocationReader.cs
@@ -9,6 +9,7 @@ using Excel = Microsoft.Office.Interop.Excel;
 using Microsoft.Office.Interop.Excel;
 using System.IO;
 using System.Reflection;
+using System.Xml;
 
 namespace LocationsForExcelXMLGenerator
 {
@@ -26,152 +27,97 @@ namespace LocationsForExcelXMLGenerator
         Excel.Worksheet worksheet = null;
         Excel.Range usedCells = null;
         static Excel.Application app = new Excel.Application();
+        
+       
 
         public void generateXML()
         {
             string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\eth.xlsx");
-
+            string pathJson = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\locations.xml");
 
             WB = app.Workbooks.Open(path);
             worksheet = WB.Worksheets[1];
             usedCells = worksheet.UsedRange;
             Console.WriteLine("Used Rows: " + usedCells.Count);
             // readSheet(); // use default values
-            read();
+            XmlWriterSettings xmlWriterSettings = new XmlWriterSettings()
+            {
+                Indent = true
+               
+            };
+            using (XmlWriter writer = XmlWriter.Create(pathJson,xmlWriterSettings))
+            {
+               
+                writer.WriteStartElement("Locations");
+                 read(writer);
+                writer.WriteEndElement();
+              
+                writer.Flush();
+                writer.Close();
+            }
+
+
+            //save json to disk
+
+            WB.Close();
+
+            Marshal.ReleaseComObject(WB);
+            Marshal.ReleaseComObject(app);
+
         }
 
 
-        private void readSheet(string label = "Region", double parentID = -99, int searchLimit = int.MaxValue)
-        {
-            Console.WriteLine(parentID);
-           
+    
+        
 
-            try
-            {
-
-                Excel.Range test = usedCells.Columns[4];
-                Excel.Range currentRegion = test.Find(parentID, Missing.Value,
-      Excel.XlFindLookIn.xlValues, Excel.XlLookAt.xlWhole,
-      Excel.XlSearchOrder.xlByRows, Excel.XlSearchDirection.xlNext, true,
-      Missing.Value, Missing.Value);
-            
-
-                Excel.Range firstFind = null;
-
-                int cnt = 0;
-
-                while (currentRegion != null)
-                {
-                    //// Keep track of the first range you find. 
-                    if (firstFind == null)
-                    {
-                        firstFind = currentRegion;
-                    }
-
-                    // If you didn't move to a new range, you are done.
-                    else if (currentRegion.get_Address(Excel.XlReferenceStyle.xlA1)
-                          == firstFind.get_Address(Excel.XlReferenceStyle.xlA1))
-                    {
-                        break;
-                    }
-                    double id = ((worksheet.Cells[currentRegion.Row, ID_COLUMN] as Excel.Range).Value);
-                    string lab = ((worksheet.Cells[currentRegion.Row, LABEL_COLUMN] as Excel.Range).Value);
-                    double pID = ((worksheet.Cells[currentRegion.Row, PARENT_COLUMN] as Excel.Range).Value);
-                    string type = ((worksheet.Cells[currentRegion.Row, TYPE_COLUMN] as Excel.Range).Value);
-                    Console.WriteLine("ID: " + id + " " + "parentID:" + " " + pID);
-                   
-                    LocationData locationData = new LocationData(id, label, "Reg", pID);
-                    Console.WriteLine(lab);
-
-                    
-                        currentRegion = test.FindNext(currentRegion);
-                    
-
-                    
-                  readSheet("Zone", id, usedCells.Count);
-                    
-
-
-
-
-                    /* if(type.Equals("Zone"))
-                     {
-                         readSheet("Woreda", id);
-                     }*/
-                   
-                    Console.WriteLine("Loop: " + cnt++);
-                }
-            
-            
-
-                   
-                       
-
-                
-            }
-
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                
-
-            }
-           
-        }
-
-        private Dictionary<LocationData, List<LocationData>> read()
+        private Dictionary<LocationData, List<LocationData>> read(XmlWriter writer)
         {
             Dictionary<LocationData, List<LocationData>> graph = new Dictionary<LocationData, List<LocationData>>();
             object[,] valueArray = (object[,])usedCells.get_Value(
                                        XlRangeValueDataType.xlRangeValueDefault);
             int rowsLength = valueArray.GetLength(0);
             int columnsLength = valueArray.GetLength(1);
-          
 
-            for (int r = 2; r <= rowsLength; r++)
-            {
-                double id =  (double)valueArray[r, ID_COLUMN];
-                string label = (string)valueArray[r, LABEL_COLUMN];
-                string type = (string)valueArray[r, TYPE_COLUMN];
-                double parent = (double)valueArray[r, PARENT_COLUMN];
-                
             
-                Console.WriteLine("Label:" + label + " Type:" + type);
-                List<LocationData> children = getChildren(r, valueArray, id);
-                graph.Add(new LocationData(id, label, type, parent), children);
-               foreach (LocationData ld in children )
-                {
-                    graph.Add(ld, getChildren(r, valueArray, ld.id));
-                }
+            getChildren(writer,2, valueArray, -99);
 
-               
-            }
+         
+              
+           // }
             return graph;
         }
 
-        private List<LocationData> getChildren(int parentRow,object[,]vals, double parentID)
+        private void getChildren(XmlWriter writer, int parentRow,object[,]vals, double parentID)
         {
             List<LocationData> locs = new List<LocationData>();
             int rowsLength = vals.GetLength(0);
-            int index = parentRow+1;
-            string firstLabel = null;
-         
-            while (index <= rowsLength)
+            int index = parentRow;
+            
+                                   
+            while (index<=rowsLength)
             {
-
                 double id = (double)vals[index, ID_COLUMN];
                 double pid = (double)vals[index, PARENT_COLUMN];
                 string label = (string)vals[index, LABEL_COLUMN];
                 string type = (string)vals[index, TYPE_COLUMN];
-               
-               
-                if (pid==parentID)
-                    locs.Add(new LocationData(id, label, type, pid));
-                index++;
-            }    
-   
+              
 
-            return locs;
+                if (pid == parentID)
+                {
+                    writer.WriteStartElement(type);
+                    writer.WriteAttributeString("ID", ((int)id).ToString());
+                    writer.WriteAttributeString("parent_ID", ((int)pid).ToString());
+                    writer.WriteAttributeString("name", label);
+
+               
+                    getChildren(writer, index, vals, id);
+
+                    writer.WriteEndElement();
+                }
+                index++;
+            }
+                          
+           
         }
         
     }
